6dc26208179656aaae26fa8db418fa8a563114aa
Esteban Hernandez <acer720db@gmail.com>
2015-11-30 18:35:07 +0000

    Removed NumReader module reference

--- spec/solution_spec.rb
+++ spec/solution_spec.rb
@@ -5,19 +5,19 @@ describe "solution.rb" do
   describe "#int_to_word" do
   
     it "returns 4527 as four thousand five hundred twenty seven" do
-      expect(NumReader.int_to_word(4527)).to eq("four thousand five hundred twenty seven")
+      expect(int_to_word(4527)).to eq("four thousand five hundred twenty seven")
     end
     
     it "returns 200017 as two hundred thousand seventeen" do
-      expect(NumReader.int_to_word(200017)).to eq("two hundred thousand seventeen")
+      expect(int_to_word(200017)).to eq("two hundred thousand seventeen")
     end
     
     it "returns 784 as seven hundred eighty four" do
-      expect(NumReader.int_to_word(784)).to eq("seven hundred eighty four")
+      expect(int_to_word(784)).to eq("seven hundred eighty four")
     end
     
     it "returns 1000215 as one million two hundred fifteen" do
-      expect(NumReader.int_to_word(1000215)).to eq("one million two hundred fifteen")
+      expect(int_to_word(1000215)).to eq("one million two hundred fifteen")
     end
     
   end
