1b4f80e69f54c5f20fab7e7ca60a7ff37f699c0f
Esteban Hernandez <acer720db@gmail.com>
2015-11-30 19:35:47 +0000

    Added result string assembly and return

--- lib/solution.rb
+++ lib/solution.rb
@@ -50,7 +50,8 @@ end
 # The int_to_word method accepts an integer above 0 and below 1 billion
 # and returns a string representation of the number in english numerics
 def int_to_word(number)
-
+  result = ""
+  
   # Dividing the number by a million returns the three digits containing millions
   million = number / 1000000
   
@@ -63,4 +64,9 @@ def int_to_word(number)
   # the hundred values
   hundred = number % 1000
   
+  result += "#{hundred_to_word(million)} million " unless million == 0
+  result += "#{hundred_to_word(thousand)} thousand " unless thousand == 0
+  result += "#{hundred_to_word(hundred)}" unless hundred == 0
+  
+  result.strip
 end
