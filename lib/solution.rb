10c8bcaa1d3dbc1208dc5c68a224cc661512ad4e
Esteban Hernandez <acer720db@gmail.com>
2015-11-30 19:06:58 +0000

    Added digit separation

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,7 +1,17 @@
 # The hundred_to_word method accepts an integer above 0 and below 1 thousand
 # and returns a string representation of the number in english numerics
 def hundred_to_word(number)
-
+  tens = {
+  
+  }
+  ones = {
+  
+  }
+  
+  h = number / 100
+  t = (number / 100) % 10
+  o = number % 10
+  
 end
 
 # The int_to_word method accepts an integer above 0 and below 1 billion
@@ -14,7 +24,7 @@ def int_to_word(number)
   # Dividing the number by a thousand returns 6 digits including the thousands
   # and hundreds. The hundred value digits are trimmed by applying the modulus
   # operation of the value of 'million'.
-  thousand = million != 0 ? (number / 1000) % million : number / 1000
+  thousand = (number / 1000) % 1000
   
   # Applying the modulus operation of 1000 returns the three digits representing
   # the hundred values
