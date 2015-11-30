429282cc07f6e47022d7df29d5685015c32eaeeb
Esteban Hernandez <acer720db@gmail.com>
2015-11-30 20:30:51 +0000

    Reduced the numerics hash spacing

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,38 +1,17 @@
 # The hundred_to_word method accepts an integer above 0 and below 1 thousand
 # and returns a string representation of the number in english numerics
 def hundred_to_word(number)
-  result = ""
-  
   numerics = {
-    1 => "one",
-    2 => "two",
-    3 => "three",
-    4 => "four",
-    5 => "five",
-    6 => "six",
-    7 => "seven",
-    8 => "eight",
-    9 => "nine",
-    10 => "ten",
-    11 => "eleven",
-    12 => "twelve",
-    13 => "thirteen",
-    14 => "fourteen",
-    15 => "fifteen",
-    16 => "sixteen",
-    17 => "seventeen",
-    18 => "eighteen",
-    19 => "nineteen",
-    20 => "twenty",
-    30 => "thirty",
-    40 => "forty",
-    50 => "fifty",
-    60 => "sixty",
-    70 => "seventy",
-    80 => "eighty",
-    90 => "ninety"
+    1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six",
+    7 => "seven", 8 => "eight", 9 => "nine", 10 => "ten", 11 => "eleven",
+    12 => "twelve", 13 => "thirteen", 14 => "fourteen", 15 => "fifteen",
+    16 => "sixteen", 17 => "seventeen", 18 => "eighteen", 19 => "nineteen",
+    20 => "twenty", 30 => "thirty", 40 => "forty", 50 => "fifty", 60 => "sixty",
+    70 => "seventy", 80 => "eighty", 90 => "ninety"
   }
 
+  result = ""
+  
   h = number / 100
   result += "#{numerics[h]} hundred " unless h == 0
   
