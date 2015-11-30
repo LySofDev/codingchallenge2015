f8077ed2e3a039e4d2cf8721c29debc71f2b15f7
Esteban Hernandez <acer720db@gmail.com>
2015-11-30 19:00:57 +0000

    Added documentation to #hundred_to_word

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,3 +1,5 @@
+# The hundred_to_word method accepts an integer above 0 and below 1 thousand
+# and returns a string representation of the number in english numerics
 def hundred_to_word(number)
 
 end
@@ -12,7 +14,7 @@ def int_to_word(number)
   # Dividing the number by a thousand returns 6 digits including the thousands
   # and hundreds. The hundred value digits are trimmed by applying the modulus
   # operation of the value of 'million'.
-  thousand = (number / 1000) % million
+  thousand = million != 0 ? (number / 1000) % million : number / 1000
   
   # Applying the modulus operation of 1000 returns the three digits representing
   # the hundred values
