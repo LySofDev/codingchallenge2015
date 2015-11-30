b5c87008b2a1516b49cf2ac8ba02b900ed1e3441
Esteban Hernandez <acer720db@gmail.com>
2015-11-30 18:54:01 +0000

    Added documentation on range separation

--- lib/solution.rb
+++ lib/solution.rb
@@ -2,8 +2,17 @@
 # and returns a string representation of the number in english numerics
 def int_to_word(number)
 
+  # Dividing the number by a million returns the three digits containing
+  # millions
   million = number / 1000000
+  
+  # Dividing the number by a thousand returns 6 digits including the thousands
+  # and hundreds. The hundred value digits are trimmed by applying the modulus
+  # operation of the value of 'million'.
   thousand = (number / 1000) % million
+  
+  # Applying the modulus operation of 1000 returns the three digits representing
+  # the hundred values
   hundred = number % 1000
   
 end
